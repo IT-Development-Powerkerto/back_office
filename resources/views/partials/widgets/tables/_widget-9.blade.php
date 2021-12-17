0d652008d70921c17865f4d12dc313d94a25372c
Rias Gauri Nurhasanah <19102212@ittelkom-pwt.ac.id>
2021-12-17 14:53:00 +0700

    new

--- resources/views/partials/widgets/tables/_widget-9.blade.php
+++ resources/views/partials/widgets/tables/_widget-9.blade.php
@@ -169,9 +169,22 @@
 																	<button type="submit" data-bs-toggle="modal" data-bs-target="#add-user" class="btn btn-outline-primary btn-xs m-3 ">
 																		Edit
 																	</button>
-																	<button type="submit" class="btn btn-danger btn-xs m-3">
-																		Delete
-																	</button>
+																	<button type="submit" class="btn btn-danger btn-xs m-3" value="Delete" data-bs-toggle="modal" data-bs-target="#delete">Delete</button>
+																	
+																</div>
+																<div class="modal fade" tabindex="-1" id="delete">
+																	<div class="modal-dialog">
+																		<div class="modal-content">
+																			<div class="modal-header">
+																				<h5 class="modal-title">Delete</h5>
+																				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+																			</div>
+																			<div class="modal-body d-flex justify-content-center">
+																			<input type="submit" class="btn btn-danger btn-xs m-3" value="Delete" data-bs-toggle="modal" data-bs-target="#delete">
+																				<button type="button" class="btn btn-outline-primary">Cancel</button>
+																			</div>
+																		</div>
+																	</div>
 																</div>
 															</td>
 														</tr>
