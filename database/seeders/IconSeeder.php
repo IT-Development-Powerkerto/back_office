c485ae6be0ddf456ae4d067ca823005f951ac15a
Elen Ardiyanto <elard.yanto@gmail.com>
2021-12-19 19:17:06 +0700

    update

--- database/seeders/IconSeeder.php
+++ database/seeders/IconSeeder.php
@@ -3,14 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-<<<<<<< HEAD
-
-class icon_seeder extends Seeder
-=======
 use Illuminate\Support\Facades\DB;
 
 class IconSeeder extends Seeder
->>>>>>> ac402e44482b2a74578500cb3f880667fe7dec6e
 {
     /**
      * Run the database seeds.
@@ -21,20 +16,6 @@ class IconSeeder extends Seeder
     {
         $icon = [
             [
-<<<<<<< HEAD
-               'icon'  => 'fa fa-genderless text-success fs-1',
-            ],
-            [
-               'icon'  => 'fa fa-genderless text-info fs-1',
-            ],
-            [
-               'icon'  => 'fa fa-genderless text-danger fs-1',
-            ],
-            [
-               'icon'  => 'fa fa-genderless text-primary fs-1',
-            ]
-        ];
-=======
                'name'  => 'fa fa-genderless text-primary fs-1',
             ],
             [
@@ -48,7 +29,6 @@ class IconSeeder extends Seeder
             ],
         ];
 
->>>>>>> ac402e44482b2a74578500cb3f880667fe7dec6e
         DB::table('icons')->insert($icon);
     }
 }
