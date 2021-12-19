12d2109e64cb878d6dce2067c76543a82453e28e
Elen Ardiyanto <elard.yanto@gmail.com>
2021-12-19 19:15:01 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -66,6 +66,7 @@ class DatabaseSeeder extends Seeder
      $this->call([
          RoleSeeder::class,
          StatusSeeder::class,
+         IconSeeder::class,
      ]);
    //   Post::create([
    //      'title' => 'Judul Pertama',
