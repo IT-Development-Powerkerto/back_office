7c752494befe137e8077a0be2e21d8eec8f203c7
Rias Gauri Nurhasanah <19102212@ittelkom-pwt.ac.id>
2021-12-21 13:55:23 +0700

    new

--- resources/views/CreateCampaign.blade.php
+++ resources/views/CreateCampaign.blade.php
@@ -131,7 +131,7 @@
 												<label for="inputtittle" class="col-form-label">Operator</label>
 											</div>
                                             <div class="col-10">
-                                                <form name="add_name" id="add_name">  
+                                                <!-- <form name="add_name" id="add_name">  
                                                     <div class="alert alert-danger print-error-msg" style="display:none">
                                                         <ul></ul>
                                                     </div>
@@ -150,6 +150,30 @@
                                                             </tr>  
                                                         </table>  
                                                     </div>
+                                                </form> -->
+                                                <form method="post" id="dynamic_form">
+                                                    <span id="result"></span>
+                                                    <table class="table table-bordered table-striped" id="user_table">
+                                                        <thead>
+                                                            <tr>
+                                                                <th width="35%">First Name</th>
+                                                                <th width="35%">Last Name</th>
+                                                                <th width="30%">Action</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+
+                                                        </tbody>
+                                                        <tfoot>
+                                                            <tr>
+                                                                <td colspan="2" align="right">&nbsp;</td>
+                                                                <td>
+                                                            @csrf
+                                                                    <input type="submit" name="save" id="save" class="btn btn-primary" value="Save" />
+                                                                </td>
+                                                            </tr>
+                                                        </tfoot>
+                                                    </table>
                                                 </form>
                                             </div>
                                         </div>
@@ -191,7 +215,7 @@
 		<script src="../assets/js/custom/widgets.js"></script>
 		<!--end::Page Custom Javascript-->
 
-        <script type="text/javascript">
+        <!-- <script type="text/javascript">
             $(document).ready(function(){      
             var postURL = "<?php echo url('addmore'); ?>";
             var i=1;  
@@ -249,6 +273,75 @@
                 });
             }
             });  
+        </script> -->
+
+
+        <script>
+            $(document).ready(function(){
+
+            var count = 1;
+
+            dynamic_field(count);
+
+            function dynamic_field(number)
+            {
+            html = '<tr>';
+                    html += '<td><input type="text" name="first_name[]" class="form-control" /></td>';
+                    html += '<td><input type="text" name="last_name[]" class="form-control" /></td>';
+                    if(number > 1)
+                    {
+                        html += '<td><button type="button" name="remove" id="" class="btn btn-danger remove">Remove</button></td></tr>';
+                        $('tbody').append(html);
+                    }
+                    else
+                    {   
+                        html += '<td><button type="button" name="add" id="add" class="btn btn-success">Add</button></td></tr>';
+                        $('tbody').html(html);
+                    }
+            }
+
+            $(document).on('click', '#add', function(){
+            count++;
+            dynamic_field(count);
+            });
+
+            $(document).on('click', '.remove', function(){
+            count--;
+            $(this).closest("tr").remove();
+            });
+
+            $('#dynamic_form').on('submit', function(event){
+                    event.preventDefault();
+                    $.ajax({
+                        url:'{{ route("dynamic-field.insert") }}',
+                        method:'post',
+                        data:$(this).serialize(),
+                        dataType:'json',
+                        beforeSend:function(){
+                            $('#save').attr('disabled','disabled');
+                        },
+                        success:function(data)
+                        {
+                            if(data.error)
+                            {
+                                var error_html = '';
+                                for(var count = 0; count < data.error.length; count++)
+                                {
+                                    error_html += '<p>'+data.error[count]+'</p>';
+                                }
+                                $('#result').html('<div class="alert alert-danger">'+error_html+'</div>');
+                            }
+                            else
+                            {
+                                dynamic_field(1);
+                                $('#result').html('<div class="alert alert-success">'+data.success+'</div>');
+                            }
+                            $('#save').attr('disabled', false);
+                        }
+                    })
+            });
+
+            });
         </script>
 		<!--end::Javascript-->
 	</body>
