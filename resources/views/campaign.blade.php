a232abe869700838fb14a248bc0e9906e435d69a
Elen Ardiyanto <elard.yanto@gmail.com>
2021-12-22 11:10:25 +0700

    update campaign

--- resources/views/campaign.blade.php
+++ resources/views/campaign.blade.php
@@ -166,7 +166,6 @@
 	'https://connect.facebook.net/en_US/fbevents.js');
 	fbq('init', '{{$campaigns->facebook_pixel}}');
 	fbq('track', 'PageView');
-	{{$campaigns->event_pixel->event_pixel}}
 	</script>
 	<noscript>
 	<img height="1" width="1" style="display:none"
@@ -186,7 +185,7 @@
 			<label for="exampleInputWhatsapp" class="form-label">No. Whatsapp</label>
 			<input type="text" name="whatsapp" class="form-control" id="exampleInputWhatsapp">
 	</div>
-	<button type="submit" class="btn btn-primary">Submit</button>
+	<button type="submit" class="btn btn-primary" onclick="{{$campaigns->event_pixel->event_pixel}}">Submit</button>
 	</form>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
