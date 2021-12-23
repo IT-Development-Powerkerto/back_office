281db7cc4c1dc0b4465d8fe915df3ebc3f433b53
Elen Ardiyanto <elard.yanto@gmail.com>
2021-12-23 16:07:57 +0700

    update

--- resources/views/campaign.blade.php
+++ resources/views/campaign.blade.php
@@ -215,7 +215,7 @@
 	<form action="https://api.pwkbackoffice.com/public/api/clients" method="POST">
     <div class="mb-3" hidden>
 			<label for="exampleInputNama" class="form-label">Campaign Id</label>
-		<input type="number" name="campaign_id" value="{{$campaigns->id}}" class="form-control" id="exampleInputNama" aria-describedby="namaHelp">
+		<input type="number" name="campaign_id" value= '{{$campaigns->id}}' class="form-control" id="exampleInputNama" aria-describedby="namaHelp">
 	</div>
 	<div class="mb-3">
 			<label for="exampleInputNama" class="form-label">Nama Lengkap</label>
