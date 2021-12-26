fa991446fc3b7661cf6ec69c68869902e80fe329
mfzall07 <mfaisalhafid@gmail.com>
2021-12-26 21:12:24 +0700

    lp & login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,94 +4,26 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <!--begin::Fonts-->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-	<!--end::Fonts-->
-	<!--begin::Page Vendor Stylesheets(used by this page)-->
-	<link href="assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
-	<!--end::Page Vendor Stylesheets-->
-	<!--begin::Global Stylesheets Bundle(used by all pages)-->
-	<link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-	<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
-	<!--end::Global Stylesheets Bundle-->
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <!-- My CSS -->
-    <link rel="stylesheet" href="css/style.css">
-
-    <!-- Google Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;700&family=Viga&display=swap" rel="stylesheet">
-
-    <title>Back Office Powerkerto</title>
+    <title>Hello, world!</title>
   </head>
   <body>
-    <div class="loginlp">
-        <div class="lp">
-            <h1><span>POWER</span>KERTO</h1>
-            <h2>Sign In</h2>
-            <p>Enter your username and password</p>
-            <div class="contents">
-                @if(session()->has('success'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        {{ session('success') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
+    <h1>Hello, world!</h1>
 
-                @if(session()->has('error'))
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ session('error') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
-                <form action="{{ route('login') }}" method="POST">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="exampleusername" class="form-label">Username</label>
-                        <input type="text" name="username" class="form-control rounded-pill" id="exampleusername" aria-describedby="emailHelp">
-                        @error('username')
-                        <div class="invalid-feedback">
-                        {{ $message }}
-                        </div>
-                        @enderror
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" name="password" class="form-control rounded-pill" id="exampleInputPassword1" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary rounded-pill text-end">Login</button>
-                </form>
-            </div>
-            <footer>
-                <p>Powered by powerkerto</p>
-            </footer>
-        </div>
-        
+    <!-- Optional JavaScript; choose one of the two! -->
 
-        <div class="lr">
-            <h1>Welcome To Powerkerto Back Office!</h1>
-            <h2>Don't be busy, be productive!</h2>
-        </div>
-    </div>
+    <button>Login</button>
 
-    
-
-
-    <!-- halo0 -->
-
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/4096ccc916.js" crossorigin="anonymous"></script>
-    <!-- End -->
-
+    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-</body>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
