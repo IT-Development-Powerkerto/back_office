9320837fd4a4735a60195ce895809eaba1904d56
mfzall07 <mfaisalhafid@gmail.com>
2021-12-27 13:37:49 +0700

    remove add staff

--- resources/views/partials/widgets/tables/_widget-user.blade.php
+++ resources/views/partials/widgets/tables/_widget-user.blade.php
@@ -7,17 +7,6 @@
 												<span class="card-label fw-bolder fs-3 mb-1">Staff</span>
 												<span class="text-muted mt-1 fw-bold fs-7">{{$users->count()}} Staff</span>
 											</h3>
-											<div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover" title="Click to add a user">
-												<a href="#" data-bs-toggle="modal" data-bs-target="#add-user" class="btn btn-sm btn-light btn-active-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_invite_friends">
-												<!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
-												<span class="svg-icon svg-icon-3">
-													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-														<rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1" transform="rotate(-90 11.364 20.364)" fill="black" />
-														<rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="black" />
-													</svg>
-												</span>
-												<!--end::Svg Icon-->Add Staff</a>
-											</div>
 											<div class="modal fade" tabindex="-1" id="add-user">
 												<div class="modal-dialog">
 													<div class="modal-content">
