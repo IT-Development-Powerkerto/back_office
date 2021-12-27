2a22af9fc4db1c4cc337f37e6fb924c0f1dccc66
mfzall07 <mfaisalhafid@gmail.com>
2021-12-27 13:40:07 +0700

    remove actions adv

--- resources/views/partials/widgets/tables/_widget-user.blade.php
+++ resources/views/partials/widgets/tables/_widget-user.blade.php
@@ -101,7 +101,6 @@
 															<th class="min-w-100px">Point</th>
 															<th class="min-w-140px">Division</th>
 															<th class="min-w-120px">Status</th>
-                                                            <th class="min-w-100px text-end">Actions</th>
 														</tr>
 													</thead>
 													<!--end::Table head-->
@@ -138,27 +137,7 @@
 																	</div>
 																</div>
 															</td>
-															<td>
-																<div class="d-flex justify-content-end flex-shrink-0" aria-label="Basic outlined example">
-                                                                    <form action="{{ route('users.edit',['user' => $user->id]) }}" method="GET">
-                                                                        @csrf
-																		<div class="btn-toolbar justify-content-between px-2" role="toolbar" aria-label="Toolbar with button groups">
-																			<div class="btn-group" role="group" aria-label="First group">
-																				<button type="submit" data-bs-toggle="modal" data-bs-target="#edit-user" class="btn btn-primary  btn-icon"><i class="la la-user-edit"></i></button>
-																			</div>
-																		</div>
-                                                                    </form>
-                                                                    <form action="{{ route('dashboard.destroy', ['dashboard'=>$user->id]) }}" method="POST">
-                                                                        @csrf
-                                                                        @method('DELETE')
-																		<div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
-																			<div class="btn-group" role="group" aria-label="First group">
-																				<button type="submit" class="btn btn-danger btn-icon" onclick="return confirm('Jadi Delete Kah ?')"><i class="la la-trash"></i></button>
-																			</div>
-																		</div>
-                                                                    </form>
-																</div>
-															</td>
+															
 														</tr>
                                                         @endforeach
 													</tbody>
