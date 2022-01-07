56326449afbc58eab6767f507e212671108bfb87
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-01-07 16:19:19 +0700

    get month - 1

--- app/Http/Controllers/OmsetController.php
+++ app/Http/Controllers/OmsetController.php
@@ -10,36 +10,101 @@ use App\Models\Omset;
 use App\Models\Campaign;
 use App\Models\Product;
 use App\Models\User;
+use GrahamCampbell\ResultType\Result;
 
 class OmsetController extends Controller
 {
     public function omset(Campaign $campaign, Product $product, User $user){
-        $month = Carbon::now()->format('Y-m');
-        $omset = Lead::where('campaign_id', $campaign->id)->where('product_id', $product->id)->where('operator_id',$user->operator->implode('id'))->sum('total_price');
 
-        // $m = Carbon::parse(DB::table('leads as l')->where('l.campaign_id', $campaign->id)->where('l.product_id', $product->id)->join('operators as o', 'o.id', '=', 'l.operator_id')->where('o.user_id', $user->id)->value('l.created_at'))->format('Y-m');
-        // if($m == $month){
-        //     $omset = DB::table('leads as l')->where('l.campaign_id', $campaign->id)->where('l.product_id', $product->id)->join('operators as o', 'o.id', '=', 'l.operator_id')->where('o.user_id', $user->id);
-        //     $max_omset = DB::table('leads as l')->where('l.campaign_id', $campaign->id)->where('l.product_id', $product->id)->join('operators as o', 'o.id', '=', 'l.operator_id')->where('o.user_id', $user->id)->where('l.status_id', 5);
-        // }
-
-        DB::table('omsets')->insert([
+        // $month = Carbon::now()->format('Y-m');
+        // ambil bulan sebelumnya
+        $month = date('n', strtotime('-1 month'));
+        // jika bulan sebelumnya adalah bulan 12, tahun -1
+        if($month == 12){
+            $year = date('Y', strtotime('-1 year'));
+        }else{
+            $year = date('Y');
+        }
+        // get omset cs per product per month
+        $omset = Lead::where('campaign_id', $campaign->id)->where('product_id', $product->id)
+            ->where('operator_id',$user->operator->implode('id'))->where('status_id', 5)
+            ->whereMonth('updated_at', $month)->whereYear('updated_at', $year)
+            ->sum('total_price');
+        // get max omset per product per month
+        $max_omset = DB::table('omsets')->whereMonth('updated_at', $month)->whereYear('updated_at', $year)->max('omset');
+        if(is_null($max_omset)){
+            $max_omset = $omset;
+        }else{
+            $max_omset = $max_omset;
+        }
+        DB::table('omsets')->updateOrInsert([
             'user_id'       => $user->id,
             'product_id'    => $product->id,
             'omset'         => $omset,
+            'created_at'  => date('Y-m-d', strtotime('- 1 month')),
+            'updated_at'  => date('Y-m-d', strtotime('- 1 month')),
         ]);
 
-        $max_omset = Omset::max('omset');
-
-        DB::table('omsets')->where('user_id', $user->id)->insert([
-            'user_id'       => $user->id,
-            'product_id'    => $product->id,
-            'omset'         => $omset,
+        DB::table('omsets')->where('user_id' , $user->id)->update([
             'omset_point'   => $omset / $max_omset ,
-            'created_at' => Carbon::now()->toDateTimeString(),
-            'updated_at' => Carbon::now()->toDateTimeString(),
+            'created_at'  => date('Y-m-d', strtotime('- 1 month')),
+            'updated_at'  => date('Y-m-d', strtotime('- 1 month')),
         ]);
+        // return $omset;
 
         return response()->json($omset/$max_omset);
+
+        // return date('Y-m-d', strtotime('- 1 month'));
+    }
+    public function all_omset(){
+
+        // $month = Carbon::now()->format('Y-m');
+        // ambil bulan sebelumnya
+        $month = date('n', strtotime('-1 month'));
+        // jika bulan sebelumnya adalah bulan 12, tahun -1
+        if($month == 12){
+            $year = date('Y', strtotime('-1 year'));
+        }else{
+            $year = date('Y');
+        }
+        // get omset cs per product per month
+        $x=array('');
+        for($product_id=1; $product_id<=5; $product_id++){
+            $x = array_push($product_id);
+
+        }
+        return $x;
+        // $omset = DB::table('leads as l')->join('operators as o', 'l.operator_id', '=', 'o.id')->where('product_id', $product_id)
+        //             // ->where('l.operator_id',$user->operator->implode('id'))
+        //             ->where('status_id', 5)
+        //             ->whereMonth('l.updated_at', $month)->whereYear('l.updated_at', $year)
+        //             ->sum('total_price');
+
+                    // DB::table('omsets')->updateOrInsert([
+                    //     'user_id'       => $user_id,
+                    //     'product_id'    => $product_id,
+                    //     'omset'         => $omset,
+                    //     'created_at'  => date('Y-m-d', strtotime('- 1 month')),
+                    //     'updated_at'  => date('Y-m-d', strtotime('- 1 month')),
+                    // ]);
+        // get max omset per product per month
+        // $max_omset = DB::table('omsets')->whereMonth('updated_at', $month)->whereYear('updated_at', $year)->max('omset');
+        // if(is_null($max_omset)){
+        //     $max_omset = $omset;
+        // }else{
+        //     $max_omset = $max_omset;
+        // }
+        
+
+        // DB::table('omsets')->where('user_id' , $user_id)->update([
+        //     'omset_point'   => $omset / $max_omset ,
+        //     'created_at'  => date('Y-m-d', strtotime('- 1 month')),
+        //     'updated_at'  => date('Y-m-d', strtotime('- 1 month')),
+        // ]);
+        // return $omset;
+
+        // return response()->json($omset/$max_omset);
+
+        // return date('Y-m-d', strtotime('- 1 month'));
     }
 }
