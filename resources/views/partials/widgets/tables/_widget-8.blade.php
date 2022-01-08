3df78b715935932936891b8d7e5c52f01c3d2ce4
elenardi <elard.yanto@gmail.com>
2022-01-08 13:57:31 +0700

    fix lead tunneling view

--- resources/views/partials/widgets/tables/_widget-8.blade.php
+++ resources/views/partials/widgets/tables/_widget-8.blade.php
@@ -4,7 +4,7 @@
 										<!--begin::Header-->
 										<div class="card-header border-0 pt-5">
 											<h3 class="card-title align-items-start flex-column">
-												<span class="card-label fw-bolder fs-3 mb-1">Lead tunneling</span>
+												<span class="card-label fw-bolder fs-3 mb-1">Lead Tunneling</span>
 												<span class="text-muted mt-1 fw-bold fs-7">{{$leads->total()}} Lead</span>
 											</h3>
 											<div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover">
