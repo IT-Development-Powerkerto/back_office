ec249f8db12448a61a5082ebc0a0290d7a2155ef
mfzall07 <mfaisalhafid@gmail.com>
2022-01-10 08:33:34 +0700

    campaign form dan wa

--- resources/views/campaign.blade.php
+++ resources/views/campaign.blade.php
@@ -235,22 +235,26 @@
 </head>
 <body>
 
-	<form action="https://api.pwkbackoffice.com/public/api/lead/{{$campaign->id}}/{{$campaign->product_id}}" method="POST">
-    <div class="mb-3" hidden>
-			<label for="exampleInputNama" class="form-label">Campaign Id</label>
-		<input type="number" name="campaign_id" value="{{$campaign->id}}" class="form-control" id="exampleInputNama" aria-describedby="namaHelp">
+	<div class="container">
+		<form action="https://api.pwkbackoffice.com/public/api/lead/{{$campaign->id}}/{{$campaign->product_id}}" method="POST">
+		<div class="mb-3" hidden>
+				<label for="exampleInputNama" class="form-label">Campaign Id</label>
+			<input type="number" name="campaign_id" value="{{$campaign->id}}" class="form-control" id="exampleInputNama" aria-describedby="namaHelp">
+		</div>
+		<div class="mb-3">
+				<label for="exampleInputNama" class="form-label">Nama Lengkap</label>
+			<input type="nama" name="name" class="form-control" id="exampleInputNama" aria-describedby="namaHelp">
+		</div>
+		<div class="mb-3">
+				<label for="exampleInputWhatsapp" class="form-label">No. Whatsapp</label>
+				<input type="text" name="whatsapp" class="form-control" id="exampleInputWhatsapp">
+		</div>
+		<i style="opacity: 50%">Banyumax.id</i>
+		<div class="d-flex justify-content-center">
+			<button type="submit" class="btn btn-primary rounded-pill" onclick="{{$campaign->event_pixel->event_pixel}}">Submit</button>
+		</div>
+		</form>
 	</div>
-	<div class="mb-3">
-			<label for="exampleInputNama" class="form-label">Nama Lengkap</label>
-		<input type="nama" name="name" class="form-control" id="exampleInputNama" aria-describedby="namaHelp">
-	</div>
-	<div class="mb-3">
-			<label for="exampleInputWhatsapp" class="form-label">No. Whatsapp</label>
-			<input type="text" name="whatsapp" class="form-control" id="exampleInputWhatsapp">
-	</div>
-	<button type="submit" class="btn btn-primary" onclick="{{$campaign->event_pixel->event_pixel}}">Submit</button>
-	</form>
-
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
@@ -264,13 +268,13 @@
 <head>
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+	<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 	<title></title>
 </head>
 <body>
 
 	<form action="https://api.pwkbackoffice.com/public/api/lead_wa/{{$campaign->id}}/{{$campaign->product_id}}" method="POST">
-		<button type="submit" class="btn btn-primary" onclick="{{$campaign->event_wa->event_pixel}}">Submit</button>
+		<button type="submit" class="btn btn-success d-flex justify-content-center rounded-pill" onclick="{{$campaign->event_wa->event_pixel}}"><i class="lab la-whatsapp pe-3" style="font-size: 24px;"></i>Submit</button>
 	</form>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
