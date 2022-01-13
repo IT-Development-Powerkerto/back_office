b279cc8e6b6d28c704373a30473767369f5e41fe
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-01-13 10:21:43 +0700

    Filter by Date

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -32,14 +32,12 @@ class DashboardController extends Controller
      */
     public function index(Request $request)
     {
-
-        $users = User::where('admin_id', auth()->user()->admin_id)->get();
-        $announcements = Announcement::where('admin_id', auth()->user()->admin_id)->get();
-        $roles = Role::all();
-        $icons = Icon::all();
-        $products = Product::where('admin_id', auth()->user()->admin_id)->get();
-        $day = Carbon::now()->format('Y-m-d');
-        $leads = DB::table('leads as l')
+        if($request->date_filter){
+            // dd($request->date_filter);
+            $day = Carbon::parse($request->date_filter)->format('Y-m-d');
+            
+            // dd($day);
+            $leads = DB::table('leads as l')
             ->join('operators as o', 'l.operator_id', '=', 'o.id')
             ->join('products as p', 'l.product_id', '=', 'p.id' )
             ->join('statuses as s', 'l.status_id', '=', 's.id')
@@ -47,10 +45,31 @@ class DashboardController extends Controller
             ->join('campaigns as cm', 'l.campaign_id', '=', 'cm.id')
             ->select('l.id as id', 'advertiser', 'c.name as client_name', 'c.whatsapp as client_wa', 'cm.cs_to_customer as text', 'o.name as operator_name', 'p.name as product_name', 'l.quantity as quantity', 'l.price as price', 'l.total_price as total_price', 'l.created_at as created_at', 'l.updated_at as updated_at', 'l.status_id as status_id', 's.name as status', 'c.updated_at as client_updated_at', 'c.created_at as client_created_at')
             ->where('l.admin_id', auth()->user()->admin_id)
-            // ->where('l.updated_at', $day)
+            ->where('l.updated_at', $day)
+            ->orderByDesc('l.id')
+            ->paginate(5);
+        } else {
+            $day = Carbon::now()->format('Y-m-d');
+            $leads = DB::table('leads as l')
+            ->join('operators as o', 'l.operator_id', '=', 'o.id')
+            ->join('products as p', 'l.product_id', '=', 'p.id' )
+            ->join('statuses as s', 'l.status_id', '=', 's.id')
+            ->join('clients as c', 'l.client_id', '=', 'c.id')
+            ->join('campaigns as cm', 'l.campaign_id', '=', 'cm.id')
+            ->select('l.id as id', 'advertiser', 'c.name as client_name', 'c.whatsapp as client_wa', 'cm.cs_to_customer as text', 'o.name as operator_name', 'p.name as product_name', 'l.quantity as quantity', 'l.price as price', 'l.total_price as total_price', 'l.created_at as created_at', 'l.updated_at as updated_at', 'l.status_id as status_id', 's.name as status', 'c.updated_at as client_updated_at', 'c.created_at as client_created_at')
+            ->where('l.admin_id', auth()->user()->admin_id)
+            ->where('l.updated_at', $day)
             ->orderByDesc('l.id')
             ->paginate(5);
             // dd($leads);
+        }
+        $users = User::where('admin_id', auth()->user()->admin_id)->get();
+        $announcements = Announcement::where('admin_id', auth()->user()->admin_id)->get();
+        $roles = Role::all();
+        $icons = Icon::all();
+        $products = Product::where('admin_id', auth()->user()->admin_id)->get();
+        
+        
         $client = Client::where('admin_id', auth()->user()->admin_id)->get();
         $campaigns = Campaign::where('admin_id', auth()->user()->admin_id)->get();
         $total_lead = DB::table('products')->where('admin_id', auth()->user()->admin_id)->pluck('lead');
