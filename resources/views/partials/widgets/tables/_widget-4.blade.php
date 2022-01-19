c082453222b27de08a998a1fad39c876576738e7
mfzall_ <mfaisalhafid@gmail.com_>
2022-01-19 14:46:46 +0700

    field create promotion

--- resources/views/partials/widgets/tables/_widget-4.blade.php
+++ resources/views/partials/widgets/tables/_widget-4.blade.php
@@ -21,7 +21,8 @@
 															<th class="min-w-225px">Product</th>
 															<th class="min-w-225px">Prmotion Name</th>
 															<th class="min-w-225px">Promotion Type</th>
-															<th class="min-w-225px text-end">Nominal Promotion</th>
+															<th class="min-w-225px">Nominal Promotion</th>
+                                                            <th class="min-w-225px text-end">Actions</th>
 														</tr>
 													</thead>
 													<!--end::Table head-->
@@ -48,10 +49,37 @@
 																</div>
 															</td>
 															<td>
-																<div class="d-flex align-items-center justify-content-end">
+																<div class="d-flex align-items-center">
 																	<h1 class="text-dark fw-normal fs-6 badge badget-light-dager">Rp. 7.000</h1>
 																</div>
 															</td>
+                                                            <td>
+                                                                <div class="d-flex flex-row justify-content-end">
+                                                                    <a href="#" class="btn btn-sm btn-light btn-active-light-primary ms-3" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
+                                                                    <!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
+                                                                    <span class="svg-icon svg-icon-5 m-0">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                                            <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="black" />
+                                                                        </svg>
+                                                                    </span>
+                                                                    <!--end::Svg Icon--></a>
+                                                                    <!--begin::Menu-->
+                                                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4" data-kt-menu="true">
+                                                                        <!--begin::Menu item-->
+                                                                        <div class="menu-item px-3">
+                                                                            <a href="" class="menu-link px-3">Edit</a>
+                                                                        </div>
+                                                                        <!--end::Menu item-->
+                                                                        <!--begin::Menu item-->
+                                                                        <div class="menu-item px-3">
+                                                                            <a href="" class="menu-link px-3" data-kt-customer-table-filter="delete_row">Delete</a>
+                                                                           
+                                                                        </div>
+                                                                        <!--end::Menu item-->
+                                                                    </div>
+                                                                    <!--end::Menu-->
+                                                                </div>
+                                                            </td>
 														</tr>
 													</tbody>
 													<!--end::Table body-->
