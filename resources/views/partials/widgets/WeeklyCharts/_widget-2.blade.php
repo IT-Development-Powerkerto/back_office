848d5441199138ef03249e969a5632a7787b1472
mfzall07 <mfaisalhafid@gmail.com>
2022-02-15 08:20:29 +0700

    w-2

--- resources/views/partials/widgets/WeeklyCharts/_widget-2.blade.php
+++ resources/views/partials/widgets/WeeklyCharts/_widget-2.blade.php
@@ -5,7 +5,7 @@
 										<div class="card-header border-0 py-3" style="background-color: #00509d;">
 											<h3 class="card-title align-items-start flex-column">
 												<span class="card-label fw-bolder fs-3 mb-1 text-white">Total Leads</span>
-												<span class="text-white fw-bold fs-7">{{$lead_week_count}} Leads @ Week</span>
+												<span class="text-white fw-bold fs-7">{{$lead_count}} Leads @ Week</span>
                                                 <input id="lead_week1" value="{{$lead_week1->count()}}" hidden/>
                                                 <input id="lead_week2" value="{{$lead_week2->count()}}" hidden/>
                                                 <input id="lead_week3" value="{{$lead_week3->count()}}" hidden/>
