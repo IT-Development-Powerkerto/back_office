ae9c9252cb812a6de77530dba575a1729d7d788d
elenardi <elard.yanto@gmail.com>
2022-02-19 14:24:26 +0700

    currency format rupiah in budgeting

--- resources/views/partials/widgets/budgeting/DailyBudgetADV.blade.php
+++ resources/views/partials/widgets/budgeting/DailyBudgetADV.blade.php
@@ -47,22 +47,86 @@
                                                                 </td>
                                                                 <td>
                                                                     <div class="d-flex align-items-center">
-                                                                        <h1 class="text-dark fw-medium fs-6">{{$lead_day->where('admin_id', auth()->user()->admin_id)->where('advertiser', $adv->name)->count()}}</h1>
+                                                                        <h1 class="text-dark fw-medium fs-6">
+                                                                            <script>
+                                                                                var bilangan = {{$lead_day->where('admin_id', auth()->user()->admin_id)->where('advertiser', $adv->name)->count()}};
+
+                                                                                var	number_string = bilangan.toString(),
+                                                                                    sisa 	= number_string.length % 3,
+                                                                                    rupiah 	= number_string.substr(0, sisa),
+                                                                                    ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+
+                                                                                if (ribuan) {
+                                                                                    separator = sisa ? '.' : '';
+                                                                                    rupiah += separator + ribuan.join('.');
+                                                                                }
+
+                                                                                document.write(rupiah);
+                                                                            </script>
+                                                                        </h1>
                                                                     </div>
                                                                 </td>
                                                                 <td>
                                                                     <div class="d-flex align-items-center">
-                                                                        <h1 class="text-dark fw-medium fs-6">{{$lead_day->where('admin_id', auth()->user()->admin_id)->where('advertiser', $adv->name)->where('status_id', 5)->count()}}</h1>
+                                                                        <h1 class="text-dark fw-medium fs-6">
+                                                                            <script>
+                                                                                var bilangan = {{$lead_day->where('admin_id', auth()->user()->admin_id)->where('advertiser', $adv->name)->where('status_id', 5)->count()}};
+
+                                                                                var	number_string = bilangan.toString(),
+                                                                                    sisa 	= number_string.length % 3,
+                                                                                    rupiah 	= number_string.substr(0, sisa),
+                                                                                    ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+
+                                                                                if (ribuan) {
+                                                                                    separator = sisa ? '.' : '';
+                                                                                    rupiah += separator + ribuan.join('.');
+                                                                                }
+
+                                                                                document.write(rupiah);
+                                                                            </script>
+                                                                        </h1>
                                                                     </div>
                                                                 </td>
                                                                 <td>
                                                                     <div class="d-flex align-items-center">
-                                                                        <h1 class="text-dark fw-medium fs-6">{{$lead_day->where('admin_id', auth()->user()->admin_id)->where('advertiser', $adv->name)->where('status_id', 5)->sum('quantity')}}</h1>
+                                                                        <h1 class="text-dark fw-medium fs-6">
+                                                                            <script>
+                                                                                var bilangan = {{$lead_day->where('admin_id', auth()->user()->admin_id)->where('advertiser', $adv->name)->where('status_id', 5)->sum('quantity')}};
+
+                                                                                var	number_string = bilangan.toString(),
+                                                                                    sisa 	= number_string.length % 3,
+                                                                                    rupiah 	= number_string.substr(0, sisa),
+                                                                                    ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+
+                                                                                if (ribuan) {
+                                                                                    separator = sisa ? '.' : '';
+                                                                                    rupiah += separator + ribuan.join('.');
+                                                                                }
+
+                                                                                document.write(rupiah);
+                                                                            </script>
+                                                                        </h1>
                                                                     </div>
                                                                 </td>
                                                                 <td>
                                                                     <div class="d-flex align-items-center justify-content-end">
-                                                                        <h1 class="text-dark fw-medium fs-6">Rp. {{$omset_day->where('admin_id', auth()->user()->admin_id)->where('adv_name', $adv->name)->sum('total_price') - $omset_day->where('admin_id', auth()->user()->admin_id)->where('adv_name', $adv->name)->sum('product_promotion')}}</h1>
+                                                                        <h1 class="text-dark fw-medium fs-6">Rp.
+                                                                            <script>
+                                                                                var bilangan = {{$omset_day->where('admin_id', auth()->user()->admin_id)->where('adv_name', $adv->name)->sum('total_price') - $omset_day->where('admin_id', auth()->user()->admin_id)->where('adv_name', $adv->name)->sum('product_promotion')}};
+
+                                                                                var	number_string = bilangan.toString(),
+                                                                                    sisa 	= number_string.length % 3,
+                                                                                    rupiah 	= number_string.substr(0, sisa),
+                                                                                    ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+
+                                                                                if (ribuan) {
+                                                                                    separator = sisa ? '.' : '';
+                                                                                    rupiah += separator + ribuan.join('.');
+                                                                                }
+
+                                                                                document.write(rupiah);
+                                                                            </script>
+                                                                        </h1>
                                                                     </div>
                                                                 </td>
                                                             </tr>
