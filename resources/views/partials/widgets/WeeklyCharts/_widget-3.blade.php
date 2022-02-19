6c981465b3cbcd22311687aecccf083a5989455a
elenardi <elard.yanto@gmail.com>
2022-02-19 12:10:39 +0700

    currency format rupiah in admin dashboard

--- resources/views/partials/widgets/WeeklyCharts/_widget-3.blade.php
+++ resources/views/partials/widgets/WeeklyCharts/_widget-3.blade.php
@@ -5,7 +5,23 @@
 										<div class="card-header border-0 py-3" style="background-color: #00509d;">
 											<h3 class="card-title align-items-start flex-column">
 												<span class="card-label fw-bolder fs-3 mb-1 text-white">Total Closing</span>
-												<span class="text-white fw-bold fs-7">{{$closing_count}} Closing @ Week</span>
+												<span class="text-white fw-bold fs-7">
+                                                    <script>
+                                                        var bilangan = {{$closing_count}};
+
+                                                        var	number_string = bilangan.toString(),
+                                                            sisa 	= number_string.length % 3,
+                                                            rupiah 	= number_string.substr(0, sisa),
+                                                            ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+
+                                                        if (ribuan) {
+                                                            separator = sisa ? '.' : '';
+                                                            rupiah += separator + ribuan.join('.');
+                                                        }
+
+                                                        document.write(rupiah);
+                                                    </script> Closing @ Week
+                                                </span>
                                                 <input id="closing_week1" value="{{$closing_week1->count()}}" hidden/>
                                                 <input id="closing_week2" value="{{$closing_week2->count()}}" hidden/>
                                                 <input id="closing_week3" value="{{$closing_week3->count()}}" hidden/>
--- resources/views/partials/widgets/charts/_widget-7.blade.php
+++ resources/views/partials/widgets/charts/_widget-7.blade.php
@@ -13,7 +13,23 @@
 												</svg>
 											</span>
 											<!--end::Svg Icon-->
-                                            <div class="text-light fw-bolder fs-2 mb-2 mt-5">Rp. {{($lead_day_count == 0) ? 0 : $advertising_day_count / $lead_day_count}}</div>
+                                            <div class="text-light fw-bolder fs-2 mb-2 mt-5">Rp.
+                                                <script>
+                                                    var bilangan = {{($lead_day_count == 0) ? 0 : $advertising_day_count / $lead_day_count}};
+
+                                                    var	number_string = bilangan.toString(),
+                                                        sisa 	= number_string.length % 3,
+                                                        rupiah 	= number_string.substr(0, sisa),
+                                                        ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+
+                                                    if (ribuan) {
+                                                        separator = sisa ? '.' : '';
+                                                        rupiah += separator + ribuan.join('.');
+                                                    }
+
+                                                    document.write(rupiah);
+                                                </script>
+                                            </div>
 											<div class="fw-bold text-light">Cost Per Result</div>
 										</div>
 										<!--end::Body-->
