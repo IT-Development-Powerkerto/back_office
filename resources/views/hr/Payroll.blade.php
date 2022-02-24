3a1b2b68c6bc20f567ce94d5db106332f643a7e6
mfzall07 <mfaisalhafid@gmail.com>
2022-02-24 10:22:31 +0700

    Overview Profile

--- resources/views/hr/Payroll.blade.php
+++ resources/views/hr/Payroll.blade.php
@@ -73,11 +73,11 @@
 							<div class="card card-xl-stretch mb-5 mb-xl-8">
 								<!--begin::Header-->
 								<div class="card-header border-0 pt-5">
-									<h3 class="card-title align-items-start flex-column">
-										<span class="card-label fw-bolder fs-3 mb-1">Management Members Shortcut</span>
-										<span class="text-muted mt-1 fw-bold fs-7">1</span>
+									<h3 class="card-title align-items-start flex-column mt-n3">
+										<span class="card-label fw-bolder fs-3 mb-1">Staff</span>
+										<span class="text-muted mt-1 fw-bold fs-7">1 Staff</span>
 									</h3>
-									<div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover">
+									<div class="card-toolbar mt-n3" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover">
 										<!--begin::Search-->
 										<div class="d-flex align-items-center position-relative my-1 me-2">
 											<!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
@@ -88,7 +88,7 @@
 												</svg>
 											</span>
 											<!--end::Svg Icon-->
-											<input type="text" data-kt-customer-table-filter="search" class="form-control form-control-solid w-250px ps-15" style="height: 33px;" placeholder="Search Users" />
+											<input type="text" data-kt-customer-table-filter="search" class="form-control form-control-solid w-250px ps-15" style="height: 33px;" placeholder="Search Staff" />
 										</div>
 										<!--end::Search-->
 										<a href="#" class="btn btn-sm btn-light btn-active-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_invite_friends">
@@ -99,30 +99,7 @@
 												<rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="black" />
 											</svg>
 										</span>
-										<!--end::Svg Icon-->New Member</a>
-										<a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm mx-2">
-											<!--begin::Svg Icon | path: icons/duotune/general/gen019.svg-->
-											<span class="svg-icon svg-icon-3">
-												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-													<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-														<polygon points="0 0 24 0 24 24 0 24"/>
-														<path d="M6.26193932,17.6476484 C5.90425297,18.0684559 5.27315905,18.1196257 4.85235158,17.7619393 C4.43154411,17.404253 4.38037434,16.773159 4.73806068,16.3523516 L13.2380607,6.35235158 C13.6013618,5.92493855 14.2451015,5.87991302 14.6643638,6.25259068 L19.1643638,10.2525907 C19.5771466,10.6195087 19.6143273,11.2515811 19.2474093,11.6643638 C18.8804913,12.0771466 18.2484189,12.1143273 17.8356362,11.7474093 L14.0997854,8.42665306 L6.26193932,17.6476484 Z" fill="#000000" fill-rule="nonzero" transform="translate(11.999995, 12.000002) rotate(-180.000000) translate(-11.999995, -12.000002) "/>
-													</g>
-												</svg>
-											</span>
-											<!--end::Svg Icon-->
-										</a>
-										<a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm">
-											<!--begin::Svg Icon | path: icons/duotune/general/gen027.svg-->
-											<span class="svg-icon svg-icon-3">
-												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-													<path d="M5 9C5 8.44772 5.44772 8 6 8H18C18.5523 8 19 8.44772 19 9V18C19 19.6569 17.6569 21 16 21H8C6.34315 21 5 19.6569 5 18V9Z" fill="black" />
-													<path opacity="0.5" d="M5 5C5 4.44772 5.44772 4 6 4H18C18.5523 4 19 4.44772 19 5V5C19 5.55228 18.5523 6 18 6H6C5.44772 6 5 5.55228 5 5V5Z" fill="black" />
-													<path opacity="0.5" d="M9 4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4V4H9V4Z" fill="black" />
-												</svg>
-											</span>
-											<!--end::Svg Icon-->
-										</a>
+										<!--end::Svg Icon-->Add Staff</a>
 									</div>
 								</div>
 								<!--end::Header-->
@@ -135,17 +112,13 @@
 											<!--begin::Table head-->
 											<thead>
 												<tr class="fw-bolder text-muted">
-													<th class="w-25px">
-														<div class="form-check form-check-sm form-check-custom form-check-solid">
-															<input class="form-check-input" type="checkbox" value="1" data-kt-check="true" data-kt-check-target=".widget-9-check" />
-														</div>
-													</th>
+													<th class="">No</th>
 													<th class="">Name</th>
-													<th class="">Email</th>
-													<th class="">Phone</th>
-													<th class="">Packet</th>
-													<th class="">Status</th>
-													<th class="">Expired In</th>
+													<th class="">Bank</th>
+													<th class="">Account Number</th>
+													<th class="">Effective Entry</th>
+													<th class="">Month's Salary</th>
+													<th class="">Level Grade</th>
 													<th class="text-end">Actions</th>
 												</tr>
 											</thead>
@@ -154,59 +127,28 @@
 											<tbody>
 												<tr>
 													<td>
-														<div class="form-check form-check-sm form-check-custom form-check-solid">
-															<input class="form-check-input widget-9-check" type="checkbox" value="1" />
-														</div>
+														<label class="text-dark fw-medium-block fs-6">1</label>
 													</td>
 													<td>
-														<div class="d-flex align-items-center">
-															<div class="d-flex justify-content-start flex-column">
-																<a href="#" class="text-dark fw-bolder text-hover-primary fs-6">Zall</a>
-																<span class="text-muted fw-bold text-muted d-block fs-7">Banyumax</span>
-															</div>
-														</div>
+														<label class="text-dark fw-medium-block fs-6">Zall</label>
 													</td>
 													<td>
-														<label class="text-dark fw-medium-block fs-6">zall@zall.com</label>
+														<label class="text-dark fw-medium-block fs-6">Mandiri</label>
 													</td>
 													<td>
-														<label class="text-dark fw-medium-block fs-6">+6281245527645</label>
+														<label class="text-dark fw-medium-block fs-6">37271827873221</label>
 													</td>
 													<td>
-														<label class="text-dark fw-medium-block fs-6">Corporate</label>
+														<label class="text-dark fw-medium-block fs-6">26 Days This Month</label>
 													</td>
 													<td>
-														<label class="text-dark badge badge-light-success fw-medium-block fs-7">Active</label>
+														<label class="text-dark fw-medium-block fs-6">Rp. 1.000.000</label>
 													</td>
 													<td>
-														<label class="text-dark badge badge-light-success fw-medium-block fs-7">27 Days Left</label>
+														<label class="text-dark fw-medium-block fs-6">Immortal</label>
 													</td>
 													<td>
 														<div class="d-flex justify-content-end flex-shrink-0">
-															<a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
-																<!--begin::Svg Icon | path: icons/duotune/general/gen019.svg-->
-																<span class="svg-icon svg-icon-3">
-																	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-																		<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																			<polygon points="0 0 24 0 24 24 0 24"/>
-																			<path d="M6.26193932,17.6476484 C5.90425297,18.0684559 5.27315905,18.1196257 4.85235158,17.7619393 C4.43154411,17.404253 4.38037434,16.773159 4.73806068,16.3523516 L13.2380607,6.35235158 C13.6013618,5.92493855 14.2451015,5.87991302 14.6643638,6.25259068 L19.1643638,10.2525907 C19.5771466,10.6195087 19.6143273,11.2515811 19.2474093,11.6643638 C18.8804913,12.0771466 18.2484189,12.1143273 17.8356362,11.7474093 L14.0997854,8.42665306 L6.26193932,17.6476484 Z" fill="#000000" fill-rule="nonzero" transform="translate(11.999995, 12.000002) rotate(-180.000000) translate(-11.999995, -12.000002) "/>
-																		</g>
-																	</svg>
-																</span>
-																<!--end::Svg Icon-->
-															</a>
-															<a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
-																<!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
-																<span class="svg-icon svg-icon-3">
-																	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-																		<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																			<polygon points="0 0 24 0 24 24 0 24"/>
-																			<path d="M6,5 L18,5 C19.6568542,5 21,6.34314575 21,8 L21,17 C21,18.6568542 19.6568542,20 18,20 L6,20 C4.34314575,20 3,18.6568542 3,17 L3,8 C3,6.34314575 4.34314575,5 6,5 Z M5,17 L14,17 L9.5,11 L5,17 Z M16,14 C17.6568542,14 19,12.6568542 19,11 C19,9.34314575 17.6568542,8 16,8 C14.3431458,8 13,9.34314575 13,11 C13,12.6568542 14.3431458,14 16,14 Z" fill="#000000"/>
-																		</g>
-																	</svg>
-																</span>
-																<!--end::Svg Icon-->
-															</a>
 															<a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
 																<!--begin::Svg Icon | path: icons/duotune/general/gen027.svg-->
 																<span class="svg-icon svg-icon-3">
