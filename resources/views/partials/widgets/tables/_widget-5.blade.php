79ec45e7b37e95fde4d79ac312c1f389775b8dcc
elenardi <elard.yanto@gmail.com>
2022-03-01 10:33:12 +0700

    add adv name in created manual lead fiture

--- resources/views/partials/widgets/tables/_widget-5.blade.php
+++ resources/views/partials/widgets/tables/_widget-5.blade.php
@@ -5,7 +5,23 @@
 										<div class="card-header border-0 pt-5">
 											<h3 class="card-title align-items-start flex-column">
 												<span class="card-label fw-bolder fs-3 mb-1">Campaign {{$campaign->title}}</span>
-												<span class="text-muted mt-1 fw-bold fs-7">{{$lead->where('campaign_id', $campaign->id)->count()}} Leads</span>
+												<span class="text-muted mt-1 fw-bold fs-7">
+                                                    <script>
+                                                        var bilangan = {{$lead->where('campaign_id', $campaign->id)->count()}};
+
+                                                        var	number_string = bilangan.toString(),
+                                                            sisa 	= number_string.length % 3,
+                                                            rupiah 	= number_string.substr(0, sisa),
+                                                            ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+
+                                                        if (ribuan) {
+                                                            separator = sisa ? '.' : '';
+                                                            rupiah += separator + ribuan.join('.');
+                                                        }
+
+                                                        document.write(rupiah);
+                                                    </script> Leads
+                                                </span>
 											</h3>
                                             <form action="/ld" method="GET" class="d-flex">
                                                 <div class="me-2 d-flex flex-row">
