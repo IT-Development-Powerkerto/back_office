40c28107bf71d2ccc62013a9ddfef45cfc994c58
mfzall07 <mfaisalhafid@gmail.com>
2022-03-02 13:11:45 +0700

    button dashboard ceo finance manager inputer reporting

--- resources/views/reporting/layout/header/_menureporting.blade.php
+++ resources/views/reporting/layout/header/_menureporting.blade.php
@@ -7,9 +7,9 @@
 	<!--begin::Menu-->
 	<div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch"
 		id="#kt_header_menu" data-kt-menu="true">
-		<div class="menu-item here show menu-lg-down-accordion me-lg-1">
-			<a class="menu-link active py-3" href="{{ route ('HumanResource.index') }}">
-				<span class="menu-title">Dashboard</span>
+		<div class="menu-item menu-lg-down-accordion me-lg-1">
+			<a class="menu-link py-3 bg-white" href="{{ route ('HumanResource.index') }}">
+				<span class="menu-title" style="color: darkblue">Dashboard</span>
 				<span class="menu-arrow d-lg-none"></span>
 			</a>
 		</div>
