13b11a2d5ffaeccef99e4cf268f89a9c9a60af3f
habibalihabsyi <habibalihabsyi123@gmail.com>
2022-03-03 11:08:03 +0700

    get additional product promo list based on product

--- public/assets/js/get-promotion-list.js
+++ public/assets/js/get-promotion-list.js
@@ -20,28 +20,15 @@ $(function(){
 			success: function(data){
 				console.log(data);
 				$.each(data.product_promotion, function(key, value){
-					// $('#product_promotion_id').append('<option value="'+value.id+'" namakota="'+ value.type +' ' +value.city_name+ '">' + value.type + ' ' + value.city_name + '</option>');
-					$('#product_promotion_id').append(`<option value=""hidden>Select Promotion</option>`);
+					$('#product_promotion_id').append(`<option value="" hidden>Select Promotion</option>`);
 					$('#product_promotion_id').append(`<option value="">Not Have Promotion</option>`);
 					$('#product_promotion_id').append(`<option value="${value.id}">${value.promotion_name}</option>`);
+					
+					$('#add_product_promotion_id').append(`<option value="" hidden>Select Promotion</option>`);
+					$('#add_product_promotion_id').append(`<option value="">Not Have Promotion</option>`);
+					$('#add_product_promotion_id').append(`<option value="${value.id}">${value.promotion_name}</option>`);
 				});
 			}
 		});
 	});
 });
-// $(function(){
-// 	$('#product').on('change',function(){
-// 		var product_id = $('#product').val();
-// 		$.ajax({
-// 			url: 'get_promotion_list/'+product_id,
-// 			type: 'GET',
-// 			dataType: 'json',
-// 			success: function(data){
-// 				console.log(data);
-// 				// $.each(data, function(key, value){
-// 				// 		$('#product_promotion_id').append('<option value="'+value.city_id+'" namakota="'+ value.type +' ' +value.city_name+ '">' + value.type + ' ' + value.city_name + '</option>');
-// 				// 	});
-// 			}
-// 		});
-// 	});
-// });
