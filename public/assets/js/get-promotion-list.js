7399646031cbcaf1d2e6ff23282a62bd1940a011
habibalihabsyi <habibalihabsyi123@gmail.com>
2022-03-03 13:52:07 +0700

    get shipping promo list based on product

--- public/assets/js/get-promotion-list.js
+++ public/assets/js/get-promotion-list.js
@@ -1,6 +1,6 @@
 $(function(){
 	$('#product_name').change(function(){
-
+		
 		var product = $('#product_name').val();
 		// console.log('produk ', {{ $products }});
 		$.ajax({
@@ -13,6 +13,16 @@ $(function(){
 			}
 		});
 		$('#product_promotion_id').empty();
+		$('#product_promotion_id').append(`<option value="" hidden>Select Promotion</option>`);
+		$('#product_promotion_id').append(`<option value="">Not Have Promotion</option>`);
+
+		$('#add_product_promotion_id').empty();
+		$('#add_product_promotion_id').append(`<option value="" hidden>Select Promotion</option>`);
+		$('#add_product_promotion_id').append(`<option value="">Not Have Promotion</option>`);
+
+		$('#shipping_promotion_id').empty();
+		$('#shipping_promotion_id').append(`<option value="" hidden>Select Promotion</option>`);
+		$('#shipping_promotion_id').append(`<option value="">Not Have Promotion</option>`);
 		$.ajax({
 			url: "get_promotion_list/"+product,
 			type: "GET",
@@ -20,13 +30,16 @@ $(function(){
 			success: function(data){
 				console.log(data);
 				$.each(data.product_promotion, function(key, value){
-					$('#product_promotion_id').append(`<option value="" hidden>Select Promotion</option>`);
-					$('#product_promotion_id').append(`<option value="">Not Have Promotion</option>`);
 					$('#product_promotion_id').append(`<option value="${value.id}">${value.promotion_name}</option>`);
-					
-					$('#add_product_promotion_id').append(`<option value="" hidden>Select Promotion</option>`);
-					$('#add_product_promotion_id').append(`<option value="">Not Have Promotion</option>`);
 					$('#add_product_promotion_id').append(`<option value="${value.id}">${value.promotion_name}</option>`);
+				});
+				
+				$.each(data.shipping_promotion, function(key, value){
+					$('#shipping_promotion_id').append(`<option value="${value.id}">${value.promotion_name}</option>`);
+					
+					// $('#add_product_promotion_id').append(`<option value="" hidden>Select Promotion</option>`);
+					// $('#add_product_promotion_id').append(`<option value="">Not Have Promotion</option>`);
+					// $('#add_product_promotion_id').append(`<option value="${value.id}">${value.promotion_name}</option>`);
 				});
 			}
 		});
