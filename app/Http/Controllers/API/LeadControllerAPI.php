463de96c045eb2332017806d9d9802aa08ff9bae
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-03-17 11:03:19 +0700

    add total lead & closing, daily lead & closing on all role

--- app/Http/Controllers/API/LeadControllerAPI.php
+++ app/Http/Controllers/API/LeadControllerAPI.php
@@ -21,12 +21,33 @@ class LeadControllerAPI extends Controller
             $day = Carbon::now()->format('Y-m-d');
         }
         if($user->role_id == 1){
-            $leads = Lead::whereDate('created_at', $day)->get();
+            $leads = Lead::whereDate('created_at', $day)->where('admin_id', $user->admin_id)->get();
+            $total_lead = Lead::where('admin_id', $user->admin_id)->count();
+            $total_closing = Lead::where('admin_id', $user->admin_id)->where('status_id', 5)->count();
+            $daily_lead = Lead::where('admin_id', $user->admin_id)->whereDate('created_at', $day)->count();
+            $daily_closing = Lead::where('admin_id', $user->admin_id)->whereDate('created_at', $day)->where('status_id', 5)->count();
         }else if($user->role_id == 5){
-            $leads = Lead::whereDate('created_at', $day)->where('user_id', $user->id)->get();
+            $leads = Lead::where('admin_id', $user->admin_id)->whereDate('created_at', $day)->where('user_id', $user->id)->get();
+            $total_lead = Lead::where('admin_id', $user->admin_id)->where('user_id', $user->id)->count();
+            $total_closing = Lead::where('admin_id', $user->admin_id)->where('user_id', $user->id)->where('status_id', 5)->count();
+            $daily_lead = Lead::where('admin_id', $user->admin_id)->where('user_id', $user->id)->whereDate('created_at', $day)->count();
+            $daily_closing = Lead::where('admin_id', $user->admin_id)->where('user_id', $user->id)->whereDate('created_at', $day)->where('status_id', 5)->count();
         }else if($user->role_id == 4){
-            $leads = Lead::where('advertiser', $user->name)->get();
+            $leads = Lead::where('admin_id', $user->admin_id)->where('advertiser', $user->name)->get();
+            $total_lead = Lead::where('admin_id', $user->admin_id)->where('advertiser', $user->name)->count();
+            $total_closing = Lead::where('admin_id', $user->admin_id)->where('advertiser', $user->name)->where('status_id', 5)->count();
+            $daily_lead = Lead::where('admin_id', $user->admin_id)->where('advertiser', $user->name)->whereDate('created_at', $day)->count();
+            $daily_closing = Lead::where('admin_id', $user->admin_id)->where('advertiser', $user->name)->whereDate('created_at', $day)->where('status_id', 5)->count();
         }
-        return LeadResource::collection($leads);
+        // $total_lead = Lead::count();
+        // $total_closing = Lead::where('status_id', 5)->count();
+        // return $total_lead;
+        return response()->json([
+            'total_lead' => $total_lead,
+            'total_closing' => $total_closing,
+            'daily_lead' => $daily_lead,
+            'daily_closing' => $daily_closing,
+            'leads' => LeadResource::collection($leads)
+        ], 200);
     }
 }
