e931768c68be7ddcbad440a676c7e1e89b5be865
Elen Ardiyanto <79313853+elenardi@users.noreply.github.com>
2022-03-30 13:55:24 +0700

    update value weekly widget in dashboard admin

--- resources/views/partials/widgets/charts/_widget-12.blade.php
+++ resources/views/partials/widgets/charts/_widget-12.blade.php
@@ -4,23 +4,7 @@
 										<!--begin::Body-->
 										<div class="card-body rounded shadow-sm" style="color:#1961A6; border:1px solid #1961A6">
 											<div class="fw-bold fs-3 mb-2 mt-5">CAQ Total</div>
-											<div class="fw-bolder fs-3hx mt-n2">
-                                                <script>
-                                                    var bilangan = {{($quantity == 0) ? 0 : round((($advertising_day_count / $quantity)/375000)*100)}};
-
-                                                    var	number_string = bilangan.toString(),
-                                                        sisa 	= number_string.length % 3,
-                                                        rupiah 	= number_string.substr(0, sisa),
-                                                        ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
-
-                                                    if (ribuan) {
-                                                        separator = sisa ? '.' : '';
-                                                        rupiah += separator + ribuan.join('.');
-                                                    }
-
-                                                    document.write(rupiah);
-                                                </script>%
-                                            </div>
+											<div class="fw-bolder fs-3hx mt-n2"> {{($quantity == 0) ? 0 : round((($advertising_day_count / $quantity)/375000)*100,1)}} %</div>
 										</div>
 										<!--end::Body-->
 									</label>
