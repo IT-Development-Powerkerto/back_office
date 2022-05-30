fddf79b547693848eea5ea32b68a9ff4bceff6cb
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-05-30 15:51:07 +0700

    warehouse only on admin

--- resources/views/layout/header/_menuADV.blade.php
+++ resources/views/layout/header/_menuADV.blade.php
@@ -43,7 +43,7 @@
 				<span class="menu-arrow d-lg-none"></span>
 			</a>
 		</div>
-		<div data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start"
+		{{-- <div data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start"
 			class="menu-item menu-lg-down-accordion me-lg-1">
 			<span class="menu-link py-3">
 				<span class="menu-title text-white">Logistics</span>
@@ -67,7 +67,7 @@
 					</span>
 				</div>
 			</div>
-		</div>
+		</div> --}}
 	</div>
 
 
@@ -121,9 +121,9 @@
 					</span>
 				</div>
 			</div>
-		</div> 
+		</div>
 	</div>--}}
-	
+
 	<!--end::Menu-->
 </div>
 <!--end::Menu wrapper-->
