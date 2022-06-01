82ee481d7f33a1783456600f224e473d8a84fc05
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-06-01 23:25:45 +0700

    sync warehouse without change value updated_at

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -11,7 +11,6 @@ class SyncController extends Controller
 {
     public function tableClient()
     {
-        // return 'asu';
         $clients = DB::table('clients')->get();
         foreach($clients as $client){
             DB::table('leads')->where('client_id', $client->id)->update([
@@ -19,48 +18,38 @@ class SyncController extends Controller
                 'client_whatsapp' => $client->whatsapp
             ]);
         }
-        // return $clients;
     }
     public function tableInputer(){
         $advs = User::where('role_id', 4)->get();
-        // return $advs;
         foreach($advs as $adv){
-            // $adv_name[] = array($adv->name);
             Inputer::where('adv_name', $adv->name)->update([
                 'adv_id' => $adv->id,
             ]);
         }
         $cs = User::whereIn('role_id', [5,13])->get();
         foreach($cs as $cs){
-            // $adv_name[] = array($adv->name);
             Inputer::where('operator_name', $cs->name)->update([
                 'cs_id' => $cs->id,
             ]);
         }
-        // $inputer = Inputer::whereIn('adv_name', $adv_name)->limit(5)->get();
-    //    $inputer = DB::table('inputers')->limit(5)->get();
-        // return $inputer;
-        // Inputer::updated([
-        //     'adv_name' =>
-        // ]);
     }
     public function warehouse()
     {
         $data_closing = Inputer::all();
         foreach ($data_closing as $d) {
             if($d->warehouse == 'Kosambi'){
-                Inputer::where('warehouse', 'Kosambi')->update([
-                    'warehouse_id' => 4
+                DB::table('inputers')->where('warehouse', 'Kosambi')->update([
+                    'warehouse_id' => 4,
                 ]);
             }
             elseif($d->warehouse == 'Tandes.Sby'){
-                Inputer::where('warehouse', 'Tandes.Sby')->update([
-                    'warehouse_id' => 3
+                DB::table('inputers')->where('warehouse', 'Tandes.Sby')->update([
+                    'warehouse_id' => 3,
                 ]);
             }
             elseif($d->warehouse == 'Cilacap'){
-                Inputer::where('warehouse', 'Cilacap')->update([
-                    'warehouse_id' => 1
+                DB::table('inputers')->where('warehouse', 'Cilacap')->update([
+                    'warehouse_id' => 1,
                 ]);
             }
         }
